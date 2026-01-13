a3a619976dd96840daa0124816aaeb7102c19827
teerapatzza <bonusltem62@gmail.com>
2026-01-13 08:11:33 +0000

    Fix assets path by moving to public

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  // ถ้าคำสั่งคือ 'build' (ตอนจะเอาขึ้น GitHub) ให้เปลี่ยน base เป็นชื่อ repo
+  if (command !== 'serve') {
+    config.base = '/loveyou/'
+  }
+
+  return config
 })
