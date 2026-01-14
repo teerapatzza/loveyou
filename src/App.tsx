3788842c3314d58448390b65c0037bcb233c74c6
teerapatzza <bonusltem62@gmail.com>
2026-01-14 03:12:47 +0000

    Add nojekyll to fix timeout

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ type BirthdayCardConfig = {
 const BIRTHDAY_CARDS: ReadonlyArray<BirthdayCardConfig> = [
   {
     id: "confetti",
-    image: "/card.png",
+    image: "card.png", // แก้แล้ว: ลบ / ออก
     position: [1, 0.081, -2],
     rotation: [-Math.PI / 2 , 0, Math.PI / 3],
   }
@@ -266,25 +266,25 @@ function AnimatedScene({
       <group ref={tableGroup}>
         <Table />
         <PictureFrame
-          image="/frame2.jpg"
+          image="frame2.jpg" // แก้แล้ว: ลบ / ออก
           position={[0, 0.735, 3]}
           rotation={[0, 5.6, 0]}
           scale={0.75}
         />
         <PictureFrame
-          image="/frame3.jpg"
+          image="frame3.jpg" // แก้แล้ว: ลบ / ออก
           position={[0, 0.735, -3]}
           rotation={[0, 4.0, 0]}
           scale={0.75}
         />
         <PictureFrame
-          image="/frame4.jpg"
+          image="frame4.jpg" // แก้แล้ว: ลบ / ออก
           position={[-1.5, 0.735, 2.5]}
           rotation={[0, 5.4, 0]}
           scale={0.75}
         />
         <PictureFrame
-          image="/frame1.jpg"
+          image="frame1.jpg" // แก้แล้ว: ลบ / ออก
           position={[-1.5, 0.735, -2.5]}
           rotation={[0, 4.2, 0]}
           scale={0.75}
@@ -381,7 +381,7 @@ export default function App() {
   const backgroundAudioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    const audio = new Audio("/music.mp3");
+    const audio = new Audio("music.mp3"); // แก้แล้ว: ลบ / ออก
     audio.loop = true;
     audio.preload = "auto";
     backgroundAudioRef.current = audio;
@@ -562,7 +562,7 @@ export default function App() {
           <ambientLight intensity={(1 - environmentProgress) * 0.8} />
           <directionalLight intensity={0.5} position={[2, 10, 0]} color={[1, 0.9, 0.95]}/>
           <Environment
-            files={["/shanghai_bund_4k.hdr"]}
+            files={["shanghai_bund_4k.hdr"]} // แก้แล้ว: ลบ / ออก
             backgroundRotation={[0, 3.3, 0]}
             environmentRotation={[0, 3.3, 0]}
             background
